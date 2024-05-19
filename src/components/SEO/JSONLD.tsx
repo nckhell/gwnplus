81496fd73eada496a198324be2b99e4e5905d578
Nick Hellemans <nick@awellhealth.com>
2024-05-19 10:47:07 +0200

    fix(): inline script

--- src/components/SEO/JSONLD.tsx
+++ src/components/SEO/JSONLD.tsx
@@ -3,6 +3,7 @@ import Script from "next/script";
 export const JSONLD = ({ data }: { data: Record<string, unknown> }) => {
   return (
     <Script
+      id="schema-jsonld"
       type="application/ld+json"
       dangerouslySetInnerHTML={{ __html: JSON.stringify(data) }}
     />
